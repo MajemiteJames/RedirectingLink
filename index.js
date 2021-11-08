42f0be50121b02c62db71e6267f07ec5c9c39d47
MajemiteJames <majemiteokoro@gmail.com>
2021-11-08 11:15:32 +0100

    fixing IOS error with map link

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function mapsSelector() {
       ((navigator.platform.indexOf("iPhone") != -1) || 
        (navigator.platform.indexOf("iPod") != -1) || 
        (navigator.platform.indexOf("iPad") != -1))
-      window.open("http://maps.google.com/maps?saddr=6.59123,3.3424&daddr=6.44468,3.412075");
-  
+      window.open("maps://maps.google.com/maps?saddr=6.59123,3.3424&daddr=6.44468,3.412075");
+
     else /* else use Google */
       window.open("http://maps.google.com/maps?saddr=6.59123,3.3424&daddr=6.44468,3.412075");
   }
