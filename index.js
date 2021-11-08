151337113c2613e9e1c2b21c6f07eca127379a61
MajemiteJames <majemiteokoro@gmail.com>
2021-11-08 08:44:23 +0100

    uploading the full project

---
+++ index.js
@@ -0,0 +1,10 @@
+function mapsSelector() {
+    if /* if we're on iOS, open in Apple Maps */
+      ((navigator.platform.indexOf("iPhone") != -1) || 
+       (navigator.platform.indexOf("iPod") != -1) || 
+       (navigator.platform.indexOf("iPad") != -1))
+      window.open("maps://maps.google.com/maps?daddr=lat,long&amp;ll=");
+  
+    else /* else use Google */
+      window.open("http://maps.google.com/maps?saddr=6.59123,3.3424&daddr=6.44468,3.412075");
+  }
