f8d49e1a35fcf4094eab01dad16e3609da2374b5
MajemiteJames <majemiteokoro@gmail.com>
2021-11-08 11:43:23 +0100

    fixing IOS error on map

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function mapsSelector() {
     if /* if we're on iOS, open in Apple Maps */
-      ((navigator.platform.indexOf("iPhone") != -1) || 
-       (navigator.platform.indexOf("iPod") != -1) || 
-       (navigator.platform.indexOf("iPad") != -1))
-      window.open("http://maps.google.com/maps?saddr=6.59123,3.3424&daddr=6.44468,3.412075");
+      ((navigator?.userAgentData?.platform.indexOf("iPhone") != -1) || 
+       (navigator?.userAgentData?.platform.indexOf("iPod") != -1) || 
+       (navigator?.userAgentData?.platform.indexOf("iPad") != -1))
+      window.open("maps://www.google.com/maps?saddr=6.59123,3.3424&daddr=6.44468,3.412075");
+      
     else /* else use Google */
       window.open("http://maps.google.com/maps?saddr=6.59123,3.3424&daddr=6.44468,3.412075");
   }
